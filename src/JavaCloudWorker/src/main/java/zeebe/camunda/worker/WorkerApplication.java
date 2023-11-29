5f24b42960abb4807fa6c1a270ab8ab24aeefff7
deepak-response <80972988+deepak-response@users.noreply.github.com>
2023-11-29 12:45:08 +0530

    Update WorkerApplication.java
    added service task and mutli instance workers

--- src/JavaCloudWorker/src/main/java/zeebe/camunda/worker/WorkerApplication.java
+++ src/JavaCloudWorker/src/main/java/zeebe/camunda/worker/WorkerApplication.java
@@ -6,6 +6,7 @@ import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
 import io.camunda.zeebe.spring.client.annotation.ZeebeVariable;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
@@ -32,6 +35,31 @@ public class WorkerApplication {
 		logger.setLevel(Level.ALL);
 		handler.setLevel(Level.ALL);
 		logger.addHandler(handler);
+	}
+
+	@ZeebeWorker(type = "charge_Credit_Card", autoComplete = true)
+	public void chargeServiceTask(final JobClient client, final ActivatedJob job) {
+		logger.info("Executing Service Task for Charge Credit Card as learning module...");
+	}
+	@ZeebeWorker(type = "demo_service_task", autoComplete = true)
+	public void demoServiceTask(final JobClient client, final ActivatedJob job, @ZeebeVariable String full_name) {
+		logger.info("Retrieving details for person " + full_name + " for demo workflow application...");
+	}
+
+	@ZeebeWorker(type = "get_customer_details", autoComplete = true)
+	public Map<String,Object> getCustomerDetails(final JobClient client, final ActivatedJob job, @ZeebeVariable String firstName, @ZeebeVariable String lastName) {
+		logger.info("Retrieving details for person " + firstName + " for demo workflow application...");
+		logger.info("Retrieving details for person " + lastName + " for demo workflow application...");
+		String editName = firstName+lastName;
+		logger.info("Retrieving details for person " + editName + " for demo workflow application...");
+		HashMap<String, Object> variables = new HashMap<>();
+		variables.put("fullName",editName);
+		return variables;
+	}
+
+	@ZeebeWorker(type = "multi_instance_customer_task", autoComplete = true)
+	public void multiInstanceCustomerTask(final JobClient client, final ActivatedJob job, @ZeebeVariable String customerCount) {
+		logger.info("Executing the Service multi_instance_customer_task for customer count..."+customerCount);
 	}
 
 	@ZeebeWorker(type = "notify_person_to_quarantine", autoComplete = true)
@@ -58,5 +86,10 @@ public class WorkerApplication {
 		logger.info("Retrieving Recovery Certificate " + recovery_certificate_uuid + " from external database...");
 		logger.info("Retrieving contact details for person " + person_uuid + "from external database...");
 		logger.info("Sending Recovery Certificate to person " + person_uuid + ". Enjoy that ice-cream!");
+	}
+
+	@ZeebeWorker(type = "multi_instance_notification", autoComplete = true)
+	public void multiInstanceNotification(final JobClient client, final ActivatedJob job) {
+		logger.info("Executing the Service Task ...");
 	}
 }
